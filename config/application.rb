f6d0c95f4010335caf9f1aed2385209dfb39171a
Dan Mundy <drm.mundy@gmail.com>
2012-10-13 15:44:37 -0400

    Adds font assets and includes them in the pipeline

--- config/application.rb
+++ config/application.rb
@@ -78,5 +78,8 @@ module LearnMeSomething
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Adds asset paths
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
