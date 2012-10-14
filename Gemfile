8bbf1986b26bde98f3ffb2cbae8750190b091aa5
Koushik Dasika <koushikdasika@yahoo.com>
2012-10-13 20:45:17 -0400

    added a portion of the tags search function in the home controller

--- Gemfile
+++ Gemfile
@@ -27,3 +27,4 @@ gem "devise", ">= 2.1.2"
 gem "devise_invitable", ">= 1.0.3"
 gem "cancan", ">= 1.6.8"
 gem "rolify", ">= 3.2.0"
+gem 'nokogiri'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,14 @@ class HomeController < ApplicationController
   def index
     @users = User.all
   end
+
+  def search
+    tags = params[:tags]
+    @request_results = []
+    tags.each do |tag|
+      request = Request.joins(:tag).where('tag.name' => tag)
+      request_results.concat request
+    end
+  end
+
 end
