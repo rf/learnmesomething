d3409c2e4e8097cecdca093880143f46db148d9c
Russ Frank <russ.frank@rutgers.edu>
2012-10-14 03:25:36 -0400

    search: working search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,32 +5,28 @@ class HomeController < ApplicationController
 
   def search
     # separate the query into tags and questions
-    query = params[:query].split.reduce ({tags: [], q: ""}) do |memo, item|
+    query = params[:query].split.reduce ({tags: [], q: []}) do |memo, item|
       if /#.*/.match item 
         memo[:tags].push item
       else
-        memo[:q] += " " + item
+        memo[:q].push item
       end
+
       memo
     end
 
+    query[:q] = query[:q].join ' '
+
     # now we have query[:tags] which is an array of tags and query[:q] which is
     # the question string if applicable
-    #puts query.inspect
-    #@data = query[:tags].reduce [] do |memo, item|
-    #  
-    #  memo.concat Request.joins(:tags).where(
-    #    'tags.name = ? AND requests.title LIKE ?',
-    #    item,
-    #    '%' + query[:q] + '%'
-    #  )
-    #end
-    @data = []
-    tags = query[:tags]
-    tags.each do |tag|
-      request = Request.joins(:tags).where('tags.name' => tag)
-      @data.concat request
+    puts query.inspect
+
+    @data = query[:tags].reduce [] do |memo, item|
+      memo.concat Request.joins(:tags).where('tags.name' => item[1..-1])
     end
-    
+
+    if not query[:q].blank?
+      @data.concat Request.find(:all, :conditions => ['title LIKE ?', "%#{query[:q]}%"])
+    end
   end
 end
--- lib/tasks/bootstrap.rake
+++ lib/tasks/bootstrap.rake
@@ -5,29 +5,42 @@ namespace :bootstrap do
       title: "What are limits?", 
       downvotes: 0,  
       description: "Please describe limits to me using small words and visual aids",
-      upvotes: 1
+      upvotes: 1,
+      user_id: 2
     )
+
+    puts limits.inspect
 
     fat = Request.create(
       title: "What is the difference between the fat arrow and regular arrow in CoffeeScript??", 
       downvotes: 0,  
       description: "I am confuse lol.",
-      upvotes: 1
+      upvotes: 1,
+      user_id: 2
     )
+
+    puts fat.inspect
 
     tofu = Request.create(
       title: "Should I eat this tofu based burrito?",
       downvotes: 0,  
       description: "I am confuse lol.",
-      upvotes: 1
+      upvotes: 0,
+      user_id: 2
     )
+
+    puts tofu.inspect
 
     math = Tag.create name: "math"
     tofutag = Tag.create name: "tofu"
 
+    puts math
+
     a = RequestTag.create request_id: limits.id, tag_id: math.id
     b = RequestTag.create request_id: fat.id, tag_id: tofutag.id
     c = RequestTag.create request_id: tofu.id, tag_id: tofutag.id
+
+    puts a
   end
 
   desc "Run all bootstrapping stuffs"
