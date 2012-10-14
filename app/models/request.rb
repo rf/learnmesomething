f4af4559ed98ed6c015cfbc41769ea696f52b641
Koushik Dasika <koushikdasika@yahoo.com>
2012-10-14 02:38:56 -0400

    I are dumb

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,14 +2,9 @@ class Request < ActiveRecord::Base
   belongs_to :user
   has_many :responses
 
-<<<<<<< HEAD
   attr_accessible :description, :downvotes, :title, :upvotes, :user_id
-=======
   has_many :request_tags
   has_many :tags, :through => :request_tags
-  
-  attr_accessible :description, :downvotes, :title, :upvotes
->>>>>>> 319f2a9... fixed the associations
 
   validates_presence_of :title
   validates_presence_of :description
